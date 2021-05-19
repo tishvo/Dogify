53060a40a23bc849a0bfda72c5078391a62843d1
tishvo <tishvo@gmail.com>
2021-05-19 15:56:05 -0400

    partway through user authentication functionality

--- client/App.jsx
+++ client/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
 import axios from 'axios';
 import Dogs from './Dogs.jsx';
+import ReactModal from 'react-modal';
 
 
 class App extends React.Component {
@@ -10,12 +12,26 @@ class App extends React.Component {
         this.state = {
             dogs: [],
             page: 2,
-            zipcode: ''
+            zipcode: '',
+            signup: false,
+            login: false,
+            signup_username: '',
+            signup_password: '',
+            login_username: '',
+            login_password: ''
         }
         this.onZipcodeChange = this.onZipcodeChange.bind(this);
         this.componentDidMount = this.componentDidMount.bind(this);
         this.moreDogsClick = this.moreDogsClick.bind(this);
         this.handleKeyPress = this.handleKeyPress.bind(this);
+        this.handleOpenModal1 = this.handleOpenModal1.bind(this);
+        this.handleCloseModal1 = this.handleCloseModal1.bind(this);
+        this.handleOpenModal2 = this.handleOpenModal2.bind(this);
+        this.handleCloseModal2 = this.handleCloseModal2.bind(this);
+        this.onSignupUsernameChange = this.onSignupUsernameChange.bind(this);
+        this.onSignupPasswordChange = this.onSignupPasswordChange.bind(this);
+        this.onLoginUsernameChange = this.onLoginUsernameChange.bind(this);
+        this.onLoginPasswordChange = this.onLoginPasswordChange.bind(this);
     }
 
     componentDidMount() {
@@ -39,12 +55,36 @@ class App extends React.Component {
         })
     }
 
+    onSignupUsernameChange(e) {
+        this.setState({
+            signup_username: e.target.value
+        })
+    }
+
+    onSignupPasswordChange(e) {
+        this.setState({
+            signup_password: e.target.value
+        })
+    }
+
+    onLoginUsernameChange(e) {
+        this.setState({
+            login_username: e.target.value
+        })
+    }
+
+    onLoginPasswordChange(e) {
+        this.setState({
+            login_password: e.target.value
+        })
+    }
+
+
     onZipcodeChange(e) {
         this.setState({
             zipcode: e.target.value
         })
     }
-
 
 
     moreDogsClick() {
@@ -94,10 +134,65 @@ class App extends React.Component {
 
     }
 
+    handleOpenModal1() {
+        this.setState({
+            signup: true
+        })
+    }
+
+    handleCloseModal1() {
+        this.setState({
+            signup: false
+        })
+        var login = {
+            user: this.state.signup_username,
+            pass: this.state.signup_password
+        }
+        console.log(login)
+    }
+
+    handleOpenModal2() {
+        this.setState({
+            login: true
+        })
+    }
+
+    handleCloseModal2() {
+        this.setState({
+            login: false
+        })
+        var login = {
+            user: this.state.login_username,
+            pass: this.state.login_password
+        }
+        console.log(login)
+    }
+
     render() {
         return (
             <div>
             <div className="landing">
+                <button className="sign-up" onClick={this.handleOpenModal1}>Sign Up</button> <button className="login" onClick={this.handleOpenModal2}>Login</button>
+                <ReactModal
+                    isOpen={this.state.signup}
+                    contentLabel="sign up modal"
+                    className="modal1"
+                    >
+                        <p className="modal_header">Sign Up</p>
+                        <div className="modal_field">Username: <input value={this.state.signup_username} onChange={this.onSignupUsernameChange}/></div>
+                        <div className="modal_field">Password: <input value={this.state.signup_password} onChange={this.onSignupPasswordChange}/></div>
+                        <div className="modal_button_div"><button className="modal_button" onClick={this.handleCloseModal1}>Sign Up</button></div>
+                    </ReactModal>
+                <ReactModal
+                    isOpen={this.state.login}
+                    contentLabel="login modal"
+                    className="modal2"
+                    >
+                        <p className="modal_header">Login</p>
+                        <div className="modal_field">Username: <input value={this.state.login_username} onChange={this.onLoginUsernameChange}/></div>
+                        <div className="modal_field">Password: <input value={this.state.login_password} onChange={this.onLoginPasswordChange}/></div>
+                        <div className="modal_button_div"><button className="modal_button" onClick={this.handleCloseModal2}>Login</button></div>
+                    </ReactModal>
                 <p className="title">Dogify<span className="subtitle">Dog adoption</span></p>
                 <div className="quote">"Dogs are our link to paradise."</div>
                 <div className="subtitle author">- Milan Kundera</div>
