803b8fe17fa1e84d2b11d370255e286b8cf985a3
tishvo <tishvo@gmail.com>
2021-05-18 21:38:28 -0400

    additional css stylings completed

--- client/App.jsx
+++ client/App.jsx
@@ -13,9 +13,9 @@ class App extends React.Component {
             zipcode: ''
         }
         this.onZipcodeChange = this.onZipcodeChange.bind(this);
-        this.searchZipcode = this.searchZipcode.bind(this);
         this.componentDidMount = this.componentDidMount.bind(this);
         this.moreDogsClick = this.moreDogsClick.bind(this);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
     }
 
     componentDidMount() {
@@ -45,28 +45,7 @@ class App extends React.Component {
         })
     }
 
-    searchZipcode(e) {
-        let zipcode = this.state.zipcode;
-        axios.get(`/dogs/${zipcode}`)
-        .then((response) => {
-            //console.log(response.data.animals)
-            var all = response.data.animals;
-            var filter = [];
-            for (var i = 0; i < all.length; i++) {
-                var current = all[i];
-                if (current.photos.length !== 0) {
-                    filter.push(current);
-                }
-            }
-            this.setState({
-                dogs: filter
-            })
-        })
-        .catch((err) => {
-            console.log(err)
-        })
 
-    }
 
     moreDogsClick() {
         axios.get(`/moredogs/${this.state.page}`)
@@ -90,6 +69,31 @@ class App extends React.Component {
         })
     }
 
+    handleKeyPress(e) {
+        if (e.key === 'Enter') {
+            let zipcode = this.state.zipcode;
+            axios.get(`/dogs/${zipcode}`)
+            .then((response) => {
+                //console.log(response.data.animals)
+                var all = response.data.animals;
+                var filter = [];
+                for (var i = 0; i < all.length; i++) {
+                    var current = all[i];
+                    if (current.photos.length !== 0) {
+                        filter.push(current);
+                    }
+                }
+                this.setState({
+                    dogs: filter
+                })
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+        }
+
+    }
+
     render() {
         return (
             <div>
@@ -97,10 +101,14 @@ class App extends React.Component {
                 <p className="title">Dogify<span className="subtitle">Dog adoption</span></p>
                 <div className="quote">"Dogs are our link to paradise."</div>
                 <div className="subtitle author">- Milan Kundera</div>
-                
                 </div>
-            <div className="mainDogs">Search by zipcode: <input type="text" value={this.state.zipcode} onChange={this.onZipcodeChange}/><button onClick={this.searchZipcode}>Search</button></div>
-            <div className="mainDogs"><button onClick={this.moreDogsClick}>See more doggos</button></div>
+            <div className="sectionTwo">
+                <p className="sectionTwo_main">Adopting is one of the best things you can do.</p>
+                <p className="sectionTwo_sub">Give a deserving dog a forever home today.</p>
+            </div>
+            <div className="meet">Meet our doggos!</div>
+            <div className="mainDogs">Search by zipcode: <input type="text" value={this.state.zipcode} onChange={this.onZipcodeChange} onKeyPress={this.handleKeyPress}/></div>
+            <div className="mainDogs"><div className="more" onClick={this.moreDogsClick}>See more doggos</div></div>
             <div className="mainDogs"><Dogs dogs={this.state.dogs}/></div>
             </div>
         );
